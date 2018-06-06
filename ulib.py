3096d42590be220a9459ea8f6a2c99175ef3bb94
samir <36499501+samirian@users.noreply.github.com>
2018-06-06 14:14:32 -0700

    Update ulib.py

--- ulib.py
+++ ulib.py
@@ -17,5 +17,9 @@ class communication :
     def ioctl(self, operation):
         fcntl.ioctl(self.file, operation)
 
-
+if __name__ == "__main__":
+    como = communication("new file.txt", "r+")
+    como.send("hello")
+    print(como.recive())
+    como.close()
 
