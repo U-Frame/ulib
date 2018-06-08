91e0d3c8a9f5e37b62f4fdbaa8cb5a0044926292
samir <36499501+samirian@users.noreply.github.com>
2018-06-08 21:29:58 +0200

    Update ulib.py

--- ulib.py
+++ ulib.py
@@ -1,4 +1,7 @@
 import fcntl
+from bitarray import bitarray
+from sys import getsizeof
+
 
 class communication :
 
@@ -8,13 +11,17 @@ class communication :
 
     def recive(self):
         file = open(self.vid+"/"+self.pid, "r")
-	data = file.read()
+	data = bitarray()
+	data.frombytes( file.read())
 	file.close()
         return data
 
-    def send(self, messege):
+    def send(self, request, requestType, value, index, size, data):
+	#print ("{0:0b}".format(data))
+	messege = bitarray( "{0:08b}".format(request) + "{0:08b}".format(requestType) + "{0:016b}".format(value) + "{0:016b}".format(index) + "{0:016b}".format(size) + "{0:0b}".format(data) )
+	#8 request - 8 requesttype- 16 value - 16 index - 16 size+ Data
         file = open(self.vid+"/"+self.pid, "a")
-        file.write(messege)
+        file.write(messege.tobytes())
 	file.close()
 
     def ioctl(self, operation):
@@ -23,6 +30,7 @@ class communication :
 	file.close()
 
 if __name__ == "__main__":
-    como = communication("new", "new.txt")
-    como.send("hello")
-    print(como.recive())
+	como = communication("new", "new.txt")
+	a = bitarray('01000010')
+	como.send(65, 66, 17219, 17476, 17733, 70)
+	print(como.recive())
