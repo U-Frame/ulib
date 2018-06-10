d89e70905fa98af488b398941d304f3c6f4ad1bc
samir <36499501+samirian@users.noreply.github.com>
2018-06-10 15:09:59 +0200

    Update ulib.py

--- ulib.py
+++ ulib.py
@@ -5,32 +5,27 @@ from sys import getsizeof
 
 class communication :
 
-    def __init__(self, vid, pid):
-	self.vid = vid
-	self.pid = pid
-
-    def recive(self):
-        file = open(self.vid+"/"+self.pid, "r")
+    def recive(self, interface, IN):
+        file = open(interface+"/"+IN, "r")
 	data = bitarray()
 	data.frombytes( file.read())
 	file.close()
         return data
 
-    def send(self, request, requestType, value, index, size, data):
-	#print ("{0:0b}".format(data))
-	messege = bitarray( "{0:08b}".format(request) + "{0:08b}".format(requestType) + "{0:016b}".format(value) + "{0:016b}".format(index) + "{0:016b}".format(size) + "{0:0b}".format(data) )
-	#8 request - 8 requesttype- 16 value - 16 index - 16 size+ Data
-        file = open(self.vid+"/"+self.pid, "a")
+    def send(self, interface, IN,request, requestType, value, index, size, data):
+	#print(len("{0:0{width}b}".format(data, width = size)))
+	messege = bitarray( "{0:08b}".format(request) + "{0:08b}".format(requestType) + "{0:016b}".format(value) + "{0:016b}".format(index) + "{0:016b}".format(size) + "{0:0{width}b}".format(data, width = size))
+        file = open(interface+"/" + IN, "a")
         file.write(messege.tobytes())
+	print(messege.tobytes())
 	file.close()
 
-    def ioctl(self, operation):
-        file = open(self.vid+"/"+self.pid, "r+")
+    def ioctl(self, interface, IN, operation):
+        file = open(interface+"/"+IN, "r+")
         fcntl.ioctl(file, operation)
 	file.close()
 
 if __name__ == "__main__":
-	como = communication("new", "new.txt")
-	a = bitarray('01000010')
-	como.send(65, 66, 17219, 17476, 17733, 70)
-	print(como.recive())
+	como = communication()
+	como.send("new", "5ra.txt",65, 66, 17219, 17476, 16, 70)
+	print(como.recive("new", "5ra.txt"))
