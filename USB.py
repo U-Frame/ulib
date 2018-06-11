65bcc70ea4993494956fe8ea291c8c8799eff1fa
samir <36499501+samirian@users.noreply.github.com>
2018-06-11 04:15:02 +0200

    Update USB.py

--- USB.py
+++ USB.py
@@ -1,10 +1,22 @@
 import Communication
 
+# os lib
+# print(os.path.isdir("/home/el"))
+# print(os.path.exists("/home/el/myfile.txt"))
 
-#00 -> control (SETUP) endpoint
-#01 -> IN endpoint
-#02 -> OUT endpoint
+# 00 -> control endpoint
+# 01 -> bulk endpoint
+# 02 -> interrupt endpoint
+# 03 -> ISOCHRONOUS endpoint
 
+CONTROL = "00"
+BULK = "01"
+INTERRUPT = "03"
+ISOCHRONOUS = "04"
+
+# 00 -> control (SETUP) endpoint
+# 01 -> IN endpoint
+# 02 -> OUT endpoint
 
 SETUP = "00"
 IN = "01"
@@ -18,22 +30,29 @@ class USB:
 		self.interface = interface
 		self.comm = Communication.communication()
 
+
 	def writeInterrupt(self, data):
-		self.comm.send(self.VID, self.PID, self.interface, OUT, "", "", "", "", "", data)
+	    	self.comm.sendData(self.VID, self.PID, self.interface, INTERRUPT, OUT, 16, data)
+
 
 	def readInterrupt(self):
-		data = self.comm.recive(self.VID, self.PID, self.interface, IN);
-		return data
+	    	data = self.comm.recive(self.VID, self.PID, self.interface, INTERRUPT, IN)
+	    	return data
+
 
 	def writeBulk(self, data):
-		self.comm.send(self.VID, self.PID, self.interface, OUT, 65, 66, 17219, 17476, 8, data)
+	    	self.comm.sendData(self.VID, self.PID, self.interface, BULK, OUT, 16, data)
+
 
 	def readBulk(self):
-		data = self.comm.recive(self.VID, self.PID, self.interface, IN);
-		return data		
+	    	data = self.comm.recive(self.VID, self.PID, self.interface, BULK, IN)
+	    	return data
+
 
 	def writeControl(self, request, requestType, value, index, size, data):
-		self.comm.send(self.VID, self.PID, self.interface, SETUP, request, requestType, value, index, size, data)
+		self.comm.sendWord(self.VID, self.PID, self.interface, SETUP, request, requestType, value, index, size)
+		self.comm.sendData(self.VID, self.PID, self.interface, size, data)
+
 
 if __name__ == "__main__":
 	usb = USB("10", "10", "15")
