e1f2a062e083e6ac9d0cf24b7919e7e3808609f5
samir <36499501+samirian@users.noreply.github.com>
2018-06-11 04:15:38 +0200

    Update Communication.py

--- Communication.py
+++ Communication.py
@@ -1,6 +1,7 @@
 import fcntl
 from bitarray import bitarray
 from sys import getsizeof
+import os
 
 
 class communication:
@@ -17,9 +18,21 @@ class communication:
 		    	print("no such endpoint ,printing device descriptor...")
 
 
-	def send(self, VID, PID, interface, endPoint, OUT, request, requestType, value, index, size, data):
+	def sendWord(self, VID, PID, interface, endPoint, OUT, request, requestType, value, index, size):
 		if os.path.isdir("dev/" + VID + "/" + PID + "/" + interface + "/" + endPoint + "/" + OUT):
-			messege = bitarray("{0:08b}".format(request) + "{0:08b}".format(requestType) + "{0:016b}".format(value) + "{0:016b}".format(index) + "{0:016b}".format(size) + "{0:0{width}b}".format(data, width=size))
+			messege = bitarray("{0:08b}".format(request) + "{0:08b}".format(requestType) + "{0:016b}".format(value) + "{0:016b}".format(index) + "{0:016b}".format(size))
+			file = open("dev/" + VID + "/" + PID + "/" + interface + "/" + endPoint + "/" + OUT, "a")
+			file.write(messege.tobytes())
+			print(messege.tobytes())
+			file.close()
+		
+		else:
+		    	print("no such endpoint ,printing device descriptor...")
+
+
+	def sendData(self, VID, PID, interface, endPoint, OUT, size, data):
+		if os.path.isdir("dev/" + VID + "/" + PID + "/" + interface + "/" + endPoint + "/" + OUT):
+			messege = bitarray("{0:0{width}b}".format(data, width=size))
 			file = open("dev/" + VID + "/" + PID + "/" + interface + "/" + endPoint + "/" + OUT, "a")
 			file.write(messege.tobytes())
 			print(messege.tobytes())
