7928991437ee7d73cfd63d15310a516903419936
samir <36499501+samirian@users.noreply.github.com>
2018-06-13 10:16:59 +0200

    Update USB.py

--- USB.py
+++ USB.py
@@ -10,10 +10,10 @@ import time
 # 03 -> ISOCHRONOUS endpoint
 
 
-CONTROL = "00"
-BULK = "01"
-INTERRUPT = "02"
-ISOCHRONOUS = "03"
+CONTROL = 0
+BULK = 1
+INTERRUPT = 2
+ISOCHRONOUS = 3
 
 
 # 00 -> control (SETUP) endpoint
@@ -21,78 +21,75 @@ ISOCHRONOUS = "03"
 # 02 -> OUT endpoint
 
 
-SETUP = "00"
-IN1 = Communication.IN + "01"
-IN2 = Communication.IN + "02"
-IN3 = Communication.IN + "03"
-IN4 = Communication.IN + "04"
-IN5 = Communication.IN + "05"
-IN6 = Communication.IN + "06"
-IN7 = Communication.IN + "07"
-IN8 = Communication.IN + "08"
-IN9 = Communication.IN + "09"
-IN10 = Communication.IN + "10"
-IN11 = Communication.IN + "11"
-IN12 = Communication.IN + "12"
-IN13 = Communication.IN + "13"
-IN14 = Communication.IN + "14"
-IN15 = Communication.IN + "15"
-IN16 = Communication.IN +"16"
+SETUP = 0
+IN0 = 100
+IN1 = 101
+IN2 = 102
+IN3 = 103
+IN4 = 104
+IN5 = 105
+IN6 = 106
+IN7 = 107
+IN8 = 108
+IN9 = 109
+IN10 = 110
+IN11 = 111
+IN12 = 112
+IN13 = 113
+IN14 = 114
+IN15 = 115
+IN16 = 116
 
-OUT1 = Communication.OUT + "01"
-OUT2 = Communication.OUT + "02"
-OUT3 = Communication.OUT + "03"
-OUT4 = Communication.OUT + "04"
-OUT5 = Communication.OUT + "05"
-OUT6 = Communication.OUT + "06"
-OUT7 = Communication.OUT + "07"
-OUT8 = Communication.OUT + "08"
-OUT9 = Communication.OUT + "09"
-OUT10 = Communication.OUT + "10"
-OUT11 = Communication.OUT + "11"
-OUT12 = Communication.OUT + "12"
-OUT13 = Communication.OUT + "13"
-OUT14 = Communication.OUT + "14"
-OUT15 = Communication.OUT + "15"
-OUT16 = Communication.OUT + "16"
+OUT0 = 0
+OUT1 = 1
+OUT2 = 2
+OUT3 = 3
+OUT4 = 4
+OUT5 = 5
+OUT6 = 6
+OUT7 = 7
+OUT8 = 8
+OUT9 = 9
+OUT10 = 10
+OUT11 = 11
+OUT12 = 12
+OUT13 = 13
+OUT14 = 14
+OUT15 = 15
+OUT16 = 16
 
 
 IOCTL_INTERRUPT_INTERVAL = 0
 
 
 class USB:
-	def __init__(self, VID, PID, interface):
-		self.VID = VID
-		self.PID = PID
+	def __init__(self, vid, pid, interface):
+		self.vid = vid
+		self.pid = pid
 		self.interface = interface
 		self.comm = Communication.communication()
-		self.interruptInterval = self.comm.ioctl(self.VID, self.PID, self.interface, 0, 0, 0, 0, 0, 0, 0, IOCTL_INTERRUPT_INTERVAL)
-
-	def writeInterrupt(self, data, OUTn):
-	    	self.comm.sendData(self.VID, self.PID, self.interface, INTERRUPT, OUTn, 16, data)
-
+		#self.interruptInterval = self.comm.ioctl(vid, pid, interface, 0, 0, 0, 0, 0, 0, IN0, IOCTL_INTERRUPT_INTERVAL)
+		self.interruptInterval = 1000
 
 	def writeInterruptHandler(self, OUTn, interval = None, callBackFunction = None):
 		if interval == None:
 			interval = self.interruptInterval
-
-		thread = threading.Thread(target = self.readInterruptCaller, args = (INn, interval, callBackFunction,))
+		thread = threading.Thread(target = self.writeInterruptCaller, args = (INn, interval, callBackFunction,))
 		thread.start()
 		return thread
-
-
+		
+		
 	def writeInterruptCaller(self, OUTn, interval, callBackFunction = None):
 		while True :
-			data = self.readInterrupt(OUTn)
+			data = self.writeInterrupt(OUTn)
 			callBackFunction(data)
 			time.sleep(interval/1000)
 
 
-	def readInterrupt(self, INn):
-	    	data = self.comm.recive(self.VID, self.PID, self.interface, INTERRUPT, INn)
-	    	return data
+	def writeInterrupt(self, size, data, OUTn):
+	    	self.comm.sendData(self.vid, self.vid, self.interface, INTERRUPT, OUTn, size, data)
 
-	
+
 	def readInterruptHandler(self, INn, interval = None, callBackFunction = None):
 		if interval == None:
 			interval = self.interruptInterval
@@ -108,12 +105,17 @@ class USB:
 			time.sleep(interval/1000)
 
 
-	def writeBulk(self, OUTn, data):
-	    	self.comm.sendData(self.VID, self.PID, self.interface, BULK, OUTn, 16, data)
+	def readInterrupt(self, INn):
+	    	data = self.comm.recive(self.vid, self.pid, self.interface, INTERRUPT, INn)
+	    	return data
+
+	
+	def writeBulk(self, OUTn, size, data):
+	    	self.comm.sendData(self.vid, self.pid, self.interface, BULK, OUTn, size, data)
 
 
 	def readBulk(self, INn):
-	    	data = self.comm.recive(self.VID, self.PID, self.interface, BULK, INn)
+	    	data = self.comm.recive(self.vid, self.pid, self.interface, BULK, INn)
 	    	return data
 
 
@@ -131,10 +133,10 @@ def callback2(data):
 
 
 if __name__ == "__main__":
-	usb = USB("10", "10", "15")
-	usb.writeBulk(OUT15, 65)
+	usb = USB(10, 10, 15)
+	usb.writeBulk(OUT15, 8, 65)
 	print(usb.readBulk(IN1))
-	thread1 = usb.readInterruptHandler(IN1, 10000, callback1)
-	thread2 = usb.readInterruptHandler(IN1, 10000, callback2)
+	thread1 = usb.readInterruptHandler(IN1, callBackFunction = callback1)
+	thread2 = usb.readInterruptHandler(IN1, callBackFunction = callback2)
 
 	time.sleep(2)
