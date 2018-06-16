3d3609030ce668badb4c46e6c1e64c2d3ac3adbe
samir <36499501+samirian@users.noreply.github.com>
2018-06-16 20:33:55 +0200

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -1,44 +1,147 @@
 #!/bin/bash
 
-
-echo $#
-if (( $# < 3 ))
+if [ $1 == "help" ]
 then
-	echo "Error: parameter missing"
-	echo "usage: manage.sh pid vid interface"
+	echo "usage: manage.sh make driver"
+	echo "Runs make command on the driver files."
+	echo ""
+	echo "usage: manage.sh load driver"
+	echo "usage: manage.sh create directory #pid #vid #interface"
+	echo ""
+	echo "Creates a directory to a specific interface."
+	echo ""
+	echo "usage: manage.sh default #vid #pid #interface"
+	echo "Runs make on driver files then loads the driver and creates a directory for the given vid, pid and interface an finally creates an interface by a default of 1 endpoint of each type."
+	echo "" 
+	echo "usage: manage.sh create node #vid #pid #interface #nodetype #direction"
+	echo "00 -> control"
+	echo "01 -> bulk"
+	echo "02 -> interrupt"
+	echo "03 -> isochronous"
+	echo "000-> OUT"
+	echo "001-> IN"
+	echo "To create a node, the two most significant digits denote to the number and the least significant digit denotes to the direction."
+	echo "ex: 010 means node number 2 and its direction is OUT."
+	echo ""
+elif [ $1 == "make" ] && [ $2 == "driver" ]
+then
+	make
 
-else
-	#specifying the vid,pid and interface
-	vid=$1
-	pid=$2
-	interface=$3
+elif [ $1 == "load" ] && [ $2 == "driver" ]
+then
+	echo "inserting the driver..."
+	sudo insmod udriver.ko
 
-	mode="666"
-	module="uframe"
-	dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
+elif [ $1 == "create" ] && [ $2 == "directory" ]
+then
+	if (( $# < 5 ))
+	then
+		echo "Error: Arguments missing."
+	else
+		#specifying the vid,pid and interface
+		vid=$3
+		pid=$4
+		interface=$5
+		dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
+		echo "creating directory: ""/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
+		#making a directory for the interface
+		mkdir -p $dir
+	fi
+elif [ $1 == "default" ]
+then 
+	if (( $# < 4 ))
+	then
+		echo "Error: Arguments missing."
+	else
+		make
+		vid=$2
+		pid=$3
+		interface=$4
 
-	#inserting the driver
-	insmod udriver.ko
-	#/sbin/insmod ./udriver.ko $* || exit 1
+		mode="666"
+		module="uframe"
+		
+		dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
 
-	#making a directory for the interface
-	mkdir -p $dir
+		echo "inserting the driver..."
+		sudo insmod udriver.ko
+		#/sbin/insmod ./udriver.ko $* || exit 1
+		
+		echo "creating directory: ""/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
+		mkdir -p $dir
 
-	#creating nodes (endpoints)
-	major=`awk "\\$2==\"$module\" {print \\$1}" /proc/devices`
+		echo "creating nodes (endpoints)..."
+		echo "00 -> control"
+		echo "01 -> bulk"
+		echo "02 -> interrupt"
+		echo "03 -> isochronous"
+		echo "000-> OUT"
+		echo "001-> IN"
+		echo ""
 
-	group="staff"
-	grep -q '^staff:' /etc/group || group="wheel"
+		major=`awk "\\$2==\"$module\" {print \\$1}" /proc/devices`
 
-	for number in {0..3}
-	do
-		mkdir -p "$dir"0"$number"
-		for num in {0..1}
+		group="staff"
+		grep -q '^staff:' /etc/group || group="wheel"
+
+		for type in {0..3}
 		do
-			echo "$dir""0$number""/""00$num" c $major $number 
-			mknod "$dir""0$number""/""00$num" c $major $number 
-			chgrp $group "$dir""0$number""/""00$num"
-			chmod $mode "$dir""0$number""/""00$num"
+			mkdir -p "$dir"0"$type"
+			for direction in {0..1}
+			do
+				echo "$dir""0$type""/""00$direction" c $major $type 
+				sudo mknod "$dir""0$type""/""00$direction" c $major $type
+				sudo chgrp $group "$dir""0$type""/""00$direction"
+				sudo chmod $mode "$dir""0$type""/""00$direction"
+			done
 		done
-	done
+	fi
+
+elif [ $1 == "create" ] && [ $2 == "node" ]
+then
+	if (( $# < 7 ))
+	then
+		echo "Error: Arguments missing."
+	else
+		#specifying the vid,pid and interface
+		vid=$3
+		pid=$4
+		interface=$5
+		type=$6
+		direction=$7
+
+		mode="666"
+		module="uframe"
+		
+		dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"$type"/"
+
+		major=`awk "\\$2==\"$module\" {print \\$1}" /proc/devices`
+
+		group="staff"
+		grep -q '^staff:' /etc/group || group="wheel"
+
+		nodeDir="$dir""$direction"
+
+		mkdir -p "$dir"
+
+		if [ $type == "00" ]
+		then 
+			minor="0"
+		elif [ $type == "01" ]
+		then
+			minor="1"
+		elif [ $type == "02" ]
+		then
+			minor="2"
+		elif [ $type == "03" ]
+		then
+			minor="3"
+		fi
+
+		echo "$nodeDir c $major $minor"
+		sudo mknod $nodeDir c $major $minor
+		sudo chgrp $group $nodeDir
+		sudo chmod $mode $nodeDir
+	fi
+
 fi
