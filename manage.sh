3dea56bccdef83de94568e248d8e63af6b88045f
samir <36499501+samirian@users.noreply.github.com>
2018-06-19 16:59:33 +0200

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -1,147 +1,101 @@
-#!/bin/bash
+import os
+import sys
 
-if [ $1 == "help" ]
-then
-	echo "usage: manage.sh make driver"
-	echo "Runs make command on the driver files."
-	echo ""
-	echo "usage: manage.sh load driver"
-	echo "usage: manage.sh create directory #pid #vid #interface"
-	echo ""
-	echo "Creates a directory to a specific interface."
-	echo ""
-	echo "usage: manage.sh default #vid #pid #interface"
-	echo "Runs make on driver files then loads the driver and creates a directory for the given vid, pid and interface an finally creates an interface by a default of 1 endpoint of each type."
-	echo "" 
-	echo "usage: manage.sh create node #vid #pid #interface #nodetype #direction"
-	echo "00 -> control"
-	echo "01 -> bulk"
-	echo "02 -> interrupt"
-	echo "03 -> isochronous"
-	echo "000-> OUT"
-	echo "001-> IN"
-	echo "To create a node, the two most significant digits denote to the number and the least significant digit denotes to the direction."
-	echo "ex: 010 means node number 2 and its direction is OUT."
-	echo ""
-elif [ $1 == "make" ] && [ $2 == "driver" ]
-then
-	make
 
-elif [ $1 == "load" ] && [ $2 == "driver" ]
-then
-	echo "inserting the driver..."
-	sudo insmod udriver.ko
+VID = sys.argv[1]
+PID = sys.argv[2]
 
-elif [ $1 == "create" ] && [ $2 == "directory" ]
-then
-	if (( $# < 5 ))
-	then
-		echo "Error: Arguments missing."
-	else
-		#specifying the vid,pid and interface
-		vid=$3
-		pid=$4
-		interface=$5
-		dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
-		echo "creating directory: ""/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
-		#making a directory for the interface
-		mkdir -p $dir
-	fi
-elif [ $1 == "default" ]
-then 
-	if (( $# < 4 ))
-	then
-		echo "Error: Arguments missing."
-	else
-		make
-		vid=$2
-		pid=$3
-		interface=$4
+file = open('uframe.h', 'r')
+data = file.readlines()
+file.close()
+data[5] = '#define VID 0x' + VID + '\n'
+data[6] = '#define PID 0x' + PID + '\n'
+file = open('uframe.h', 'w')
+file.write("".join(data))
+file.close()
 
-		mode="666"
-		module="uframe"
-		
-		dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
+os.system("make")
+os.system("sudo insmod udriver.ko")
 
-		echo "inserting the driver..."
-		sudo insmod udriver.ko
-		#/sbin/insmod ./udriver.ko $* || exit 1
-		
-		echo "creating directory: ""/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"
-		mkdir -p $dir
+interface = 0
+os.system("lsusb > lsusb.txt")
+file = open('lsusb.txt', 'r')
+data = file.readlines()
+file.close()
+os.system("rm lsusb.txt")
+target = "ID " + VID + ":" + PID
 
-		echo "creating nodes (endpoints)..."
-		echo "00 -> control"
-		echo "01 -> bulk"
-		echo "02 -> interrupt"
-		echo "03 -> isochronous"
-		echo "000-> OUT"
-		echo "001-> IN"
-		echo ""
+deviceDetails = ""
+for line in data:
+	if target in line:
+		deviceDetails = line
+		break
 
-		major=`awk "\\$2==\"$module\" {print \\$1}" /proc/devices`
+if deviceDetails != "":
+	bus = deviceDetails[4:7]
+	device = deviceDetails[15:18]
 
-		group="staff"
-		grep -q '^staff:' /etc/group || group="wheel"
+	print("bus: " + bus)
+	print("device: " + device)
+	
+	os.system("lsusb -D /dev/bus/usb/" + bus + "/" + device + " > deviceDiscriptor.txt")
+	
+	file = open("deviceDiscriptor.txt", "r")
+	data = file.readlines()
+	file.close()
+	#os.system("rm deviceDiscriptor.txt")
 
-		for type in {0..3}
-		do
-			mkdir -p "$dir"0"$type"
-			for direction in {0..1}
-			do
-				echo "$dir""0$type""/""00$direction" c $major $type 
-				sudo mknod "$dir""0$type""/""00$direction" c $major $type
-				sudo chgrp $group "$dir""0$type""/""00$direction"
-				sudo chmod $mode "$dir""0$type""/""00$direction"
-			done
-		done
-	fi
+	target = "Endpoint Descriptor:"
+	ep = open("endpoints.txt", "w")
+	ep.write(target + "\n")
+	deviceDetails = ""
+	counter = 0
+	for line in data:
+		if target in line:
+			
+			for i in range(counter + 1, len(data)-1) :
+				ep.write(data[i])
+			ep.close()
+			break
+		counter += 1
 
-elif [ $1 == "create" ] && [ $2 == "node" ]
-then
-	if (( $# < 7 ))
-	then
-		echo "Error: Arguments missing."
-	else
-		#specifying the vid,pid and interface
-		vid=$3
-		pid=$4
-		interface=$5
-		type=$6
-		direction=$7
+	
+	endpointCounter = 0 
+	type = ""
+	direction = ""
+	#creating control endpoint
+	os.system("bash create_node.sh " + " " + VID + " " + PID + " " + str(interface) + " " + "Control" + " " + "{0:03}".format(0) + " " + str(endpointCounter))
 
-		mode="666"
-		module="uframe"
-		
-		dir="/home/samir/Documents/dev/fframe/"$vid"/"$pid"/"$interface"/"$type"/"
+	target = "Endpoint Descriptor:"
+	ep = open("endpoints.txt", "r")
+	data = ep.readlines()
+	counter = 0
+	for line in data:
+		if target in line:
+			type = ""
+			direction = ""
+			for i in range(counter + 1, len(data)-1) :
+				if "bEndpointAddress" in data[i]:
+					endpointCounter += 1
+					if "IN" in data[i]:
+						direction = "{0:03}".format(1 + endpointCounter*10) #IN
+					else:
+						direction = "{0:03}".format(0 + endpointCounter*10) #OUT
+				if "Transfer Type" in data[i]:
+					if "Interrupt":
+						type = "Interrupt"
+					elif "Bulk":
+						type = "Bulk"
+					elif "Isochronous":
+						type = "Isochronous"
+				if type != "" and direction != "":
+					os.system("bash create_node.sh " + " " + VID + " " + PID + " " + str(interface) + " " + type + " " + direction + " " + str(endpointCounter))
+					break
+		counter += 1
 
-		major=`awk "\\$2==\"$module\" {print \\$1}" /proc/devices`
 
-		group="staff"
-		grep -q '^staff:' /etc/group || group="wheel"
+else :
+	print ("device is not connected")
 
-		nodeDir="$dir""$direction"
 
-		mkdir -p "$dir"
 
-		if [ $type == "00" ]
-		then 
-			minor="0"
-		elif [ $type == "01" ]
-		then
-			minor="1"
-		elif [ $type == "02" ]
-		then
-			minor="2"
-		elif [ $type == "03" ]
-		then
-			minor="3"
-		fi
-
-		echo "$nodeDir c $major $minor"
-		sudo mknod $nodeDir c $major $minor
-		sudo chgrp $group $nodeDir
-		sudo chmod $mode $nodeDir
-	fi
-
-fi
