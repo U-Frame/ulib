16f94d7a6493ceb32d3430c44c406f74c9126108
sayed12212 <37817226+sayed12212@users.noreply.github.com>
2018-06-21 01:30:48 +0200

    Update communication.py

--- communication.py
+++ communication.py
@@ -13,21 +13,20 @@ class Communication:
     def __init__(self, path):
         if os.path.exists(path):
             self.path = path
+            buffer = array.array(struct.pack("iiiii", 0, 0, 0, 0, 0))
+            buffer = self.device_control(0, buffer)
+            self.type = buffer[0]
+            self.direction = buffer[1]
+            self.endpoint_address = buffer[2]
+            self.interval = buffer[3]
+            self.buffer_size = buffer[4]
         else:
             raise ValueError("Path does not exist")
 
-    def send(self, data, size=0):
+    def send(self, data):
         file = open(self.path, "w")
-        file.write(data,size)
+        file.write(data)
         file.close()
-
-    def device_control(self, byte_buffer, data, operation):
-        for d in data:
-            byte_buffer.append(d)
-        temp_buffer = array.array("B", byte_buffer)
-        buffer = array.array("c", temp_buffer.tostring())
-        fcntl.ioctl(self.path, buffer, operation)
-        return buffer[8:].tostring()
 
     def receive(self, size=0):
         file = open(self.path, "r")
@@ -35,50 +34,37 @@ class Communication:
         file.close()
         return data
 
-    def ioctl
+    def device_control(self, operation, buffer):
+        file = open(self.path, "r")
+        fcntl.ioctl(file, buffer, operation)
+        file.close()
+        return buffer
 
 
 class Bulk(Communication):
-
-    def __init__(self, vid, pid, interface, endpoint, endpoint_type="bulk"):
+    def __init__(self, vid, pid, interface, endpoint, endpoint_type="Bulk"):
         self.path = "/home/sayed/dev/" + "/" + vid + "/" + pid + "/" + interface + "/" + endpoint_type + "/" + "{0:03}".format(
             endpoint)
         super().__init__(self.path)
 
-    def send(self, data):
-        super().send(data)
-
-    def receive(self):
-        return super().receive()
-
 
 class Interrupt(Bulk):
     def __init__(self, vid, pid, interface, endpoint):
-        super().__init__(vid, pid, interface, endpoint, "interrupt")
-        structure = struct.pack("i", 0)
-        node_file = open(self.path, "r")
-        buffer = array.array("i", structure)
-        operation = 0
-        fcntl.ioctl(node_file, operation, buffer, 1)
-        node_file.close()
-        self.interrupt_interval = buffer[0]
+        super().__init__(vid, pid, interface, endpoint, "Interrupt")
 
     def write_interrupt_handler(self, callback_function=None):
-        thread = threading.Thread(target=self.__write_interrupt_caller, args=(self.path, self.interrupt_interval
-                                                                            , callback_function))
+        thread = threading.Thread(target=self.__write_interrupt_caller, args=(self.path, self.interval, callback_function))
         thread.start()
         return thread
 
-    def __write_interrupt_caller(self, callBack_function=None):
+    def __write_interrupt_caller(self, callback_function=None):
         while True:
-            data = callBack_function()
-            # self.writeInterrupt(len(data), self.path, data)
-            self.send(data,self.size)
-            time.sleep(self.interrupt_interval / 1000)
+            data = callback_function()
+            self.send(data)
+            time.sleep(self.interval / 1000.0)
 
     def read_interrupt_handler(self, callback_function=None):
-        thread = threading.Thread(target=self.__read_interrupt_caller, args=(self.path, self.interrupt_interval
-                                                                           , callback_function))
+        thread = threading.Thread(target=self.__read_interrupt_caller, args=callback_function)
         thread.start()
         return thread
 
@@ -86,7 +72,7 @@ class Interrupt(Bulk):
         while True:
             data = self.receive()
             callback_function(data)
-            time.sleep(self.interrupt_interval / 1000.0)
+            time.sleep(self.interval / 1000.0)
 
 
 class Control(Communication):
@@ -103,16 +89,14 @@ class Control(Communication):
 
     def send(self, request, request_type, value, index, size, data):
         messege = ""
-        if request is not None:
-            req = self.form_request_packet(request, request_type, value, index, size)
-            messege = messege + str(req)
-            messege = messege + data
+        req = self.form_request_packet(request, request_type, value, index, size)
+        messege = messege + str(req)
+        messege = messege + data
         super().send(messege)
 
     def receive(self, request, request_type, value, index, size, data):
-        if request is not None:
-            byte_buffer = bytearray(self.form_request_packet(request, request_type, value, index, size))
-            return super().device_control(self.path, byte_buffer, data, config.IOCTL_CONTROL_READ)
+        buffer = array.array(struct.pack("ccHHH{width}s".format(width=size), request, request_type, value, index, size, data))
+        return super().device_control(1, buffer)[8:]
 
 
 if __name__ == '__main__':
